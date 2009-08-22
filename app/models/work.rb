92ca745400e222b63d53988bd8cc30d073b5cc77
Timothy McGlynn <like_weeds@timothy-mcglynns-imac.local>
2009-08-22 12:46:18 -0400

    Added category model.

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,7 @@
 class Work < ActiveRecord::Base
   belongs_to :user
   has_many :votes
+  belongs_to :category
   
   validates_presence_of :category_id, :title, :body, :user_id
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :categories
+
    
   # Restful Authentication Rewrites
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
