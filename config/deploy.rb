da4a30aaf7e165c3d832023c0d591393e3472186
John Hutchinson <johnhutch@iMac-2.local>
2009-08-22 01:45:14 -0400

    Trying to get the deploy to work.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,3 @@
-set :stages, %w(staging production)
-set :default_stage, "production"
-require File.expand_path("#{File.dirname(__FILE__)}/../vendor/gems/capistrano-ext-1.2.1/lib/capistrano/ext/multistage")
-
 set :user, 'swiftkickpress'  # Your dreamhost account's username
 set :domain, 'swiftkickpress.com'  # Dreamhost servername where your account is located 
 set :project, 'swiftkickpress'  # Your application as its called in the repository
@@ -42,37 +38,4 @@ task :after_symlink do
   run "ln -nsf #{shared_path}/uploads/avatars #{current_path}/public/avatars"
   run "ln -nsf #{shared_path}/uploads/illustrations #{current_path}/public/illustrations"
   run "ln -nsf #{shared_path}/uploads/works #{current_path}/public/works"
-end
-
-namespace :db do
-  desc 'Dumps the production database to db/production_data.sql on the remote server'
-  task :remote_db_dump, :roles => :db, :only => { :primary => true } do
-    run "cd #{deploy_to}/#{current_dir} && " +
-      "rake RAILS_ENV=#{rails_env} db:database_dump --trace" 
-  end
-
-  desc 'Downloads db/production_data.sql from the remote production environment to your local machine'
-  task :remote_db_download, :roles => :db, :only => { :primary => true } do  
-    execute_on_servers(options) do |servers|
-      self.sessions[servers.first].sftp.connect do |tsftp|
-        tsftp.download!("#{deploy_to}/#{current_dir}/db/production_data.sql", "db/production_data.sql")
-      end
-    end
-  end
-
-  desc 'Cleans up data dump file'
-  task :remote_db_cleanup, :roles => :db, :only => { :primary => true } do
-    execute_on_servers(options) do |servers|
-      self.sessions[servers.first].sftp.connect do |tsftp|
-        tsftp.remove! "#{deploy_to}/#{current_dir}/db/production_data.sql" 
-      end
-    end
-  end 
-
-  desc 'Dumps, downloads and then cleans up the production data dump'
-  task :remote_db_runner do
-    remote_db_dump
-    remote_db_download
-    remote_db_cleanup
-  end
 end
