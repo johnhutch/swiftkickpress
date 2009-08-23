1594e056def7c33570399c55c5225e221192144e
John Hutchinson <johnhutch@john-hutchinsons-macbook.local>
2009-08-23 04:16:24 -0400

    Tested the score SQL query in work.rb.  Fixtures info added

--- app/models/work.rb
+++ app/models/work.rb
@@ -4,4 +4,16 @@ class Work < ActiveRecord::Base
   belongs_to :category
   
   validates_presence_of :category_id, :title, :body, :user_id
+
+  
+  def score
+    sql = "SELECT COUNT(*) " +
+      "FROM votes v " +
+      "JOIN roles_users r " +
+      "USING (user_id)" +
+      "WHERE v.work_id=" + self.id.to_s
+    
+    Work.count_by_sql(sql)
+  end
+
 end
